11a9612f1b5d39d0bd6e7b8acf6ea7b1cfc0b5a9
FaizBano <159723889+FaizBano@users.noreply.github.com>
2024-04-23 10:48:14 +0500

    Add files via upload
    my new project of currency converter in this project I tried to make a program to concert the different countries currencies in different values

---
+++ curency_convert.ts
@@ -0,0 +1,45 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+import { clearScreenDown } from "readline";
+const currency: any = 
+{ 
+usd: 1, //base currency
+eur: 0.99,
+gbp: 0.76,
+inr: 74.57,
+pkr: 280
+};
+let user_ans = await inquirer.prompt([
+    {
+    name: "from",
+    message: "enter from currency",
+    type: "list",
+    choices: ["usd", "eur", "gbp", "inr", "pkr"]
+},
+{ 
+    name: "to",
+    message : "enter to currency",
+    type: "list",
+    choices:["usd", "eur", "gbp", "inr", "pkr"]
+
+},
+{
+    name: "amount",
+    message: "enter your amount",
+    type: "number"
+}
+]);
+console.log(user_ans);
+
+let fromCurrency = currency[user_ans.from];
+console.log(`${user_ans.from} ${fromCurrency}`);
+let toCurrency = currency[user_ans.to];
+console.log(`${user_ans.to} ${toCurrency}`);
+let amount = user_ans.amount;
+console.log(`amount =   ${amount}`);
+let baseAmount = amount / fromCurrency;
+console.log(`your base amount will be in ${user_ans.to} is ${baseAmount}`);
+let convertedAmount =  toCurrency * amount;
+console.log(`your converted amount in ${user_ans.to} is ${baseAmount}`);
+
+
